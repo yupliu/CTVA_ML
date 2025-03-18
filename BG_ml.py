ce9d8f9012f458baa01ff619d852b81fb30f8a6f
yupliu <yupliu@users.noreply.github.com>
2025-03-18 10:51:41 -0400

    Done

--- BG_ml.py
+++ BG_ml.py
@@ -10,8 +10,112 @@ BGFile_low = r"C:\Users\am7574\OneDrive - Corteva\Documents\Projects\CDMLG\BG\20
 BGMol_low = PandasTools.LoadSDF(BGFile_low)
 BGMol_low["activity"] = 0
 BGMol_all =  pd.concat([BGMol_high, BGMol_low])
-BGMol_all = BGMol_all.sample(frac=1)
+BGMol_all = BGMol_all.sample(frac=1).reset_index(drop=True)
+BGMol_all = BGMol_all[["ROMol","activity"]]
+BGMol_all = BGMol_all.dropna()
 
+def calcFingerprint(stFile: str) -> pd.DataFrame:
+    from rdkit.Chem import PandasTools
+    sdData = PandasTools.LoadSDF(stFile)
+    from rdkit.Chem import rdFingerprintGenerator
+    mfpGen = rdFingerprintGenerator.GetMorganGenerator()
+    dataMol = sdData[["ROMol"]]
+    dataMol = sdData.dropna()
+    dataMol["MolFP"] = sdData["ROMol"].apply(lambda x:mfpGen.GetCountFingerprintAsNumPy(x))
+    return dataMol
+
+def calcProp(stFile: str) -> pd.DataFrame:
+    from rdkit.Chem import PandasTools
+    sdData = PandasTools.LoadSDF(stFile)
+    dataMol = sdData[["ROMol"]]
+    dataMol = sdData.dropna()
+    from rdkit.ML.Descriptors import MoleculeDescriptors
+    calc = MoleculeDescriptors.MolecularDescriptorCalculator([x[0] for x in rdkit.Chem.Descriptors._descList])
+    dataMol["prop"] = sdData["ROMol"].apply(lambda x:calc.CalcDescriptors(x))
+    return dataMol
+
+def calcFingerprint(mols: pd.DataFrame) -> pd.DataFrame:
+    from rdkit.Chem import rdFingerprintGenerator
+    mfpGen = rdFingerprintGenerator.GetMorganGenerator()
+    mols["MolFP"] = mols["ROMol"].apply(lambda x:mfpGen.GetCountFingerprintAsNumPy(x))
+    return mols
+
+def calcProp(mols: pd.DataFrame) -> pd.DataFrame:
+    from rdkit.ML.Descriptors import MoleculeDescriptors
+    calc = MoleculeDescriptors.MolecularDescriptorCalculator([x[0] for x in rdkit.Chem.Descriptors._descList])
+    mols["prop"] = mols["ROMol"].apply(lambda x:calc.CalcDescriptors(x))
+    return mols
+
+def assessModel(y_test, y_pred):
+    from sklearn.metrics import f1_score
+    print(f1_score(y_test, y_pred))
+    from sklearn.metrics import accuracy_score
+    print(accuracy_score(y_test, y_pred))
+    from sklearn.metrics import confusion_matrix
+    print(confusion_matrix(y_test, y_pred))
+    from sklearn.metrics import classification_report
+    print(classification_report(y_test, y_pred))
+
+def getBestModel(X_train, y_train, X_test, y_test):
+    from sklearn import pipeline
+    from sklearn.model_selection import GridSearchCV
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.ensemble import GradientBoostingClassifier
+    from sklearn.ensemble import AdaBoostClassifier
+    from sklearn.svm import SVC
+    import numpy as np    
+    pipe = pipeline.Pipeline([('classifier', RandomForestClassifier())])
+    search_space = [{'classifier': [RandomForestClassifier()],
+                     'classifier__n_estimators': [10, 100, 1000],
+                     'classifier__max_features': [1, 2, 3]},
+                    {'classifier': [LogisticRegression(solver='liblinear')],
+                     'classifier__penalty': ['l1', 'l2'],
+                     'classifier__C': np.logspace(0, 4, 10)},
+                    {'classifier': [GradientBoostingClassifier()],
+                     'classifier__n_estimators': [10, 100, 1000],
+                     'classifier__learning_rate': [0.001, 0.01, 0.1],
+                     'classifier__max_depth': [1, 2, 3]},
+                     {'classifier': [AdaBoostClassifier()]},
+                        {'classifier': [SVC()],
+                        'classifier__C': [0.1, 1, 10],
+                        'classifier__gamma': [1, 0.1, 0.01],
+                        'classifier__kernel': ['rbf', 'poly', 'sigmoid']},
+                    ]
+    
+    clf = GridSearchCV(pipe, search_space, cv=5, verbose=0, n_jobs=-1)
+    best_model = clf.fit(X_train, y_train)
+    y_pred = best_model.predict(X_test)
+    assessModel(y_test, y_pred)
+    return best_model
+
+def getBestfeatures(X, y):
+    from sklearn.feature_selection import SelectKBest, f_classif
+    X_new = SelectKBest(f_classif, k=2).fit_transform(X, y)
+    print(X_new.shape)
+    return X_new
+
+from sklearn.model_selection import train_test_split
+X_fp = calcFingerprint(BGMol_all)
+X = X_fp["MolFP"].to_list()
+y = BGMol_all['activity'].to_list()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+best_model = getBestModel(X_train, y_train, X_test, y_test)
+print(best_model.best_estimator_)
+getBestfeatures(X, y)
+
+
+import matplotlib.pyplot as plt
+plt.hist(y_test, bins=2)
+plt.hist(y_train, bins=2)
+
+X_prop = calcProp(BGMol_all)
+X = X_prop["prop"].to_list()
+y = BGMol_all['activity'].to_list()
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)   
+getBestModel(X_train, y_train, X_test, y_test)  
+getBestfeatures(X, y)
 
 
 
--- TK_model.py
+++ TK_model.py
@@ -1,21 +1,15 @@
 import rdkit
 from rdkit.Chem import PandasTools
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import rdkit
 
 stFile = r"C:\Users\am7574\OneDrive - Corteva\Documents\Projects\AI_chemistry\TK_AI\TK_ALL.sdf"
 data = PandasTools.LoadSDF(stFile)
 dataMol = data[["ROMol","IC50 uM"]]
 dataMol = dataMol.dropna()
 dataMol['activity'] = dataMol['IC50 uM'].apply(lambda x: 0 if x == 'NI' or float(x) >= 20 else 1)
-
-from rdkit.Chem import rdFingerprintGenerator
-mfpGen = rdFingerprintGenerator.GetMorganGenerator()
-dataMol["MolFP"] = dataMol["ROMol"].apply(lambda x:mfpGen.GetCountFingerprintAsNumPy(x))
-
-import sklearn as sl
-from sklearn.model_selection import train_test_split
-X = dataMol['MolFP'].to_list()
-y = dataMol['activity'].to_list()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 def assessModel(y_test, y_pred):
     from sklearn.metrics import f1_score
@@ -27,70 +21,105 @@ def assessModel(y_test, y_pred):
     from sklearn.metrics import classification_report
     print(classification_report(y_test, y_pred))
 
-from sklearn.ensemble import RandomForestClassifier
-clf = RandomForestClassifier(n_estimators=100, max_depth=2,random_state=0) 
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
+def calcFingerprint(stFile: str) -> pd.DataFrame:
+    from rdkit.Chem import PandasTools
+    sdData = PandasTools.LoadSDF(stFile)
+    from rdkit.Chem import rdFingerprintGenerator
+    mfpGen = rdFingerprintGenerator.GetMorganGenerator()
+    dataMol = sdData[["ROMol"]]
+    dataMol = sdData.dropna()
+    dataMol["MolFP"] = sdData["ROMol"].apply(lambda x:mfpGen.GetCountFingerprintAsNumPy(x))
+    return dataMol
 
-assessModel(y_test, y_pred)
+def calcProp(stFile: str) -> pd.DataFrame:
+    from rdkit.Chem import PandasTools
+    sdData = PandasTools.LoadSDF(stFile)
+    dataMol = sdData[["ROMol"]]
+    dataMol = sdData.dropna()
+    from rdkit.ML.Descriptors import MoleculeDescriptors
+    calc = MoleculeDescriptors.MolecularDescriptorCalculator([x[0] for x in rdkit.Chem.Descriptors._descList])
+    dataMol["prop"] = sdData["ROMol"].apply(lambda x:calc.CalcDescriptors(x))
+    return dataMol
 
-from sklearn.svm import SVC
-clf = SVC()
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-assessModel(y_test, y_pred)
+def calcFingerprint(mols: pd.DataFrame) -> pd.DataFrame:
+    from rdkit.Chem import rdFingerprintGenerator
+    mfpGen = rdFingerprintGenerator.GetMorganGenerator()
+    mols["MolFP"] = mols["ROMol"].apply(lambda x:mfpGen.GetCountFingerprintAsNumPy(x))
+    return mols
 
-from sklearn.linear_model import LogisticRegression
-clf = LogisticRegression() 
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-assessModel(y_test, y_pred)
+def calcProp(mols: pd.DataFrame) -> pd.DataFrame:
+    from rdkit.ML.Descriptors import MoleculeDescriptors
+    calc = MoleculeDescriptors.MolecularDescriptorCalculator([x[0] for x in rdkit.Chem.Descriptors._descList])
+    mols["prop"] = mols["ROMol"].apply(lambda x:calc.CalcDescriptors(x))
+    return mols
 
-from sklearn.ensemble import GradientBoostingClassifier
-clf = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0,max_depth=1, random_state=0)
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-assessModel(y_test, y_pred)
+def assessModel(y_test, y_pred):
+    from sklearn.metrics import f1_score
+    print(f1_score(y_test, y_pred))
+    from sklearn.metrics import accuracy_score
+    print(accuracy_score(y_test, y_pred))
+    from sklearn.metrics import confusion_matrix
+    print(confusion_matrix(y_test, y_pred))
+    from sklearn.metrics import classification_report
+    print(classification_report(y_test, y_pred))
 
-from sklearn.ensemble import AdaBoostClassifier
-clf = AdaBoostClassifier(n_estimators=100, random_state=0)
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-assessModel(y_test, y_pred)
+def getBestModel(X_train, y_train, X_test, y_test):
+    from sklearn import pipeline
+    from sklearn.model_selection import GridSearchCV
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.ensemble import GradientBoostingClassifier
+    from sklearn.ensemble import AdaBoostClassifier
+    from sklearn.svm import SVC
+    import numpy as np    
+    pipe = pipeline.Pipeline([('classifier', RandomForestClassifier())])
+    search_space = [{'classifier': [RandomForestClassifier()],
+                     'classifier__n_estimators': [10, 100, 1000],
+                     'classifier__max_features': [1, 2, 3]},
+                    {'classifier': [LogisticRegression(solver='liblinear')],
+                     'classifier__penalty': ['l1', 'l2'],
+                     'classifier__C': np.logspace(0, 4, 10)},
+                    {'classifier': [GradientBoostingClassifier()],
+                     'classifier__n_estimators': [10, 100, 1000],
+                     'classifier__learning_rate': [0.001, 0.01, 0.1],
+                     'classifier__max_depth': [1, 2, 3]},
+                     {'classifier': [AdaBoostClassifier()]},
+                        {'classifier': [SVC()],
+                        'classifier__C': [0.1, 1, 10],
+                        'classifier__gamma': [1, 0.1, 0.01],
+                        'classifier__kernel': ['rbf', 'poly', 'sigmoid']},
+                    ]
+    
+    clf = GridSearchCV(pipe, search_space, cv=5, verbose=0, n_jobs=-1)
+    best_model = clf.fit(X_train, y_train)
+    y_pred = best_model.predict(X_test)
+    assessModel(y_test, y_pred)
+    return best_model
 
-from sklearn.model_selection import GridSearchCV
-from sklearn import pipeline
+def getBestfeatures(X,y):
+    from sklearn.feature_selection import SelectKBest, mutual_info_classif
+    X_new = SelectKBest(mutual_info_classif, k=10).fit_transform(X, y)
+    print(X_new.shape)
+    return X_new
 
-import numpy as np
-pipe = pipeline.Pipeline([('classifier' , RandomForestClassifier())])
-param_grid = [
-    {'classifier' : [LogisticRegression()],
-     'classifier__penalty' : ['l1', 'l2'],
-    'classifier__C' : np.logspace(-4, 4, 20),
-    'classifier__solver' : ['liblinear']},
-    {'classifier' : [RandomForestClassifier()],
-    'classifier__n_estimators' : list(range(10,101,10)),
-    'classifier__max_features' : list(range(6,32,5))},
-    {'classifier' : [GradientBoostingClassifier()]},
-    {'classifier' : [AdaBoostClassifier()]},
-    {'classifier' : [SVC()]}
-]
+X_fp = calcFingerprint(dataMol)
+X = X_fp["MolFP"].to_list()
+y = dataMol['activity'].to_list()
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)   
+getBestModel(X_train, y_train, X_test, y_test)
+X_new = getBestfeatures(X, y)
+X_train, X_test, y_train, y_test = train_test_split(X_new, y, test_size=0.3, random_state=42)   
+getBestModel(X_train, y_train, X_test, y_test)
 
-gs = GridSearchCV(pipe, param_grid = param_grid, cv = 5, verbose=True, n_jobs=-1)
-best_clf = gs.fit(X_train, y_train)
-y_pred = best_clf.predict(X_test)
-assessModel(y_test, y_pred)
 
-from rdkit.Chem import Descriptors
-from rdkit.ML.Descriptors import MoleculeDescriptors
 
-desc_names = [x[0] for x in Descriptors._descList]
-calculator = MoleculeDescriptors.MolecularDescriptorCalculator(desc_names)
-dataMol["desc"] = dataMol["ROMol"].apply(lambda x:calculator.CalcDescriptors(x))
 
-X1 = dataMol['desc'].to_list()
-X_train, X_test, y_train, y_test = train_test_split(X1, y, test_size=0.3, random_state=42)
-gs = GridSearchCV(pipe, param_grid = param_grid, cv = 5, verbose=True, n_jobs=-1)
-best_clf = gs.fit(X_train, y_train)
-y_pred = best_clf.predict(X_test)
-assessModel(y_test, y_pred)
+
+""" X_prop = calcProp(dataMol)
+X = X_prop["prop"].to_list()
+y = dataMol['activity'].to_list()
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)   
+getBestModel(X_train, y_train, X_test, y_test)
+X_new = getBestfeatures(X, y) """
